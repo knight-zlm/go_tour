852956a4a64a26f51ea269bc195cf266275ebb2b
周礼明 <zhouliming@shihuo.cn>
2020-08-24 09:28:00 +0800

    增加配置文件读取功能

--- blogService/go.mod
+++ blogService/go.mod
@@ -12,6 +12,6 @@ require (
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.4.0 // indirect
+	github.com/spf13/viper v1.4.0
 	golang.org/x/sys v0.0.0-20200821140526-fda516888d29 // indirect
 )
--- blogService/main.go
+++ blogService/main.go
@@ -1,20 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
+
+	"github.com/knight-zlm/blog-service/global"
 	"github.com/knight-zlm/blog-service/internal/routers"
+	"github.com/knight-zlm/blog-service/pkg/setting"
 )
 
+func init() {
+	err := SetUpSetting()
+	if err != nil {
+		log.Fatalf("init.setupsetting err:%v\n", err)
+	}
+	fmt.Printf("%#v\n", global.ServerSetting)
+	fmt.Printf("%#v\n", global.AppSetting)
+	fmt.Printf("%#v\n", global.DatabaseSetting)
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func SetUpSetting() error {
+	s, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
