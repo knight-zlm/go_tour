ba658abcce5c7f15375c88cee81a1ddf0dbd7010
周礼明 <zhouliming@shihuo.cn>
2020-09-28 09:52:18 +0800

    添加调用taghttp的方法

--- tagService/main.go
+++ tagService/main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/knight-zlm/tag-service/proto"
+	"github.com/knight-zlm/tag-service/server"
+)
+
+var port string
+
+func init() {
+	flag.StringVar(&port, "p", "8008", "启动端口号")
+	flag.Parse()
+}
+
 //protoc --go_out=plugins=grpc:. ./proto/*.proto 编译proto
 func main() {
+	s := grpc.NewServer()
+	pb.RegisterTagServiceServer(s, server.NewTagServer())
 
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("s.Serve err: %v", err)
+	}
 }
