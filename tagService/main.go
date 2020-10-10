07530cf63defa1d45000317f050e7873d5afb982
周礼明 <zhouliming@shihuo.cn>
2020-10-10 09:48:41 +0800

    rpc通过不同端口支持不同协议

--- tagService/main.go
+++ tagService/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net"
+	"net/http"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -12,26 +13,55 @@ import (
 	"github.com/knight-zlm/tag-service/server"
 )
 
-var port string
+var (
+	grpcPort string
+	httpPort string
+)
 
 func init() {
-	flag.StringVar(&port, "p", "8009", "启动端口号")
+	flag.StringVar(&grpcPort, "grpc_port", "8009", "grpc启动端口号")
+	flag.StringVar(&httpPort, "http_port", "9009", "http启动端口号")
 	flag.Parse()
 }
 
 //protoc --go_out=plugins=grpc:. ./proto/*.proto 编译proto
 func main() {
+	errsChan := make(chan error)
+	go func() {
+		err := RunHttpServer(httpPort)
+		if err != nil {
+			errsChan <- err
+		}
+	}()
+	go func() {
+		err := RunGrpcServer(grpcPort)
+		if err != nil {
+			errsChan <- err
+		}
+	}()
+
+	select {
+	case err := <-errsChan:
+		log.Fatalf("Run Server err: %v", err)
+	}
+}
+
+func RunHttpServer(port string) error {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+	return http.ListenAndServe(":"+port, serveMux)
+}
+
+func RunGrpcServer(port string) error {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
-
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatalf("net.Listen err: %v", err)
+		return err
 	}
 
-	err = s.Serve(lis)
-	if err != nil {
-		log.Fatalf("s.Serve err: %v", err)
-	}
+	return s.Serve(lis)
 }
