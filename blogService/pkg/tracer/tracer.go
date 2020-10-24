a251357df907b292e645d9e502287f39e7d044ad
zhoulm <zhoulm@inmindglobal.com>
2020-10-24 11:29:21 +0800

    添加jeager说明

--- blogService/pkg/tracer/tracer.go
+++ blogService/pkg/tracer/tracer.go
@@ -8,6 +8,22 @@ import (
 	"github.com/uber/jaeger-client-go/config"
 )
 
+/*
+docs
+https://www.jaegertracing.io/docs/1.20/getting-started/#all-in-one
+docker run -d --name jaeger \
+  -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 \
+  -p 5775:5775/udp \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 14268:14268 \
+  -p 14250:14250 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one:latest
+*/
+
 func NewJaegerTracer(serviceName, agentHostPort string) (opentracing.Tracer, io.Closer, error) {
 	cfg := config.Configuration{
 		ServiceName: serviceName,
