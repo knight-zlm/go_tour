0978cdbb8934a593adeafb8a0f376ef16d7e056d
周礼明 <zhouliming@shihuo.cn>
2020-10-28 10:18:20 +0800

    设置etcd负载均衡

--- tagService/client/client.go
+++ tagService/client/client.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
-	"google.golang.org/grpc/metadata"
-
-	"github.com/knight-zlm/tag-service/internal/middleware"
-
-	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpcRetry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/resolver"
 
 	pb "github.com/knight-zlm/tag-service/proto"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/clientv3/naming"
+	grpcMiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpcRetry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	"github.com/knight-zlm/tag-service/internal/middleware"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 )
 
 type Auth struct {
@@ -38,7 +43,8 @@ func main() {
 	opts := []grpc.DialOption{grpc.WithPerRPCCredentials(&auth)}
 	md := metadata.New(map[string]string{"go": "programming", "tour": "book"})
 	newCtx := metadata.NewOutgoingContext(ctx, md)
-	clientConn, err := GetClientConn(newCtx, "localhost:8004", opts)
+	//clientConn, err := GetClientConn(newCtx, "localhost:8004", opts)
+	clientConn, err := GetClientConn2(newCtx, "tag-service", opts)
 	if err != nil {
 		log.Fatalf("err: %v\n", err)
 	}
@@ -86,5 +92,23 @@ func main1() {
 
 func GetClientConn(ctx context.Context, target string, opts []grpc.DialOption) (*grpc.ClientConn, error) {
 	opts = append(opts, grpc.WithInsecure())
+	return grpc.DialContext(ctx, target, opts...)
+}
+
+func GetClientConn2(ctx context.Context, serverName string, opts []grpc.DialOption) (*grpc.ClientConn, error) {
+	config := clientv3.Config{
+		Endpoints:   []string{"http://localhost:2379"},
+		DialTimeout: time.Second * 60,
+	}
+	cli, err := clientv3.New(config)
+	if err != nil {
+		return nil, err
+	}
+
+	r := naming.GRPCResolver{Client: cli}
+	target := fmt.Sprintf("/etcdv3://go_tour/grpc/%s", serverName)
+	resolve, _ := r.Resolve(target)
+	resolver.Register(resolve)
+	opts = append(opts, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)), grpc.WithBlock())
 	return grpc.DialContext(ctx, target, opts...)
 }
