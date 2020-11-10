dfba64c0f07a7cb3fbc0d51af1ded0287040fb9f
周礼明 <zhouliming@shihuo.cn>
2020-11-10 23:15:57 +0800

    添加测试代码

--- chatroom/cmd/chatroom/main.go
+++ chatroom/cmd/chatroom/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	addr   = ":2020"
+	banner = `
+    ____              _____
+   |    |    |   /\     |
+   |    |____|  /  \    | 
+   |    |    | /----\   |
+   |____|    |/      \  |
+Go语言编程之旅 —— 一起用Go做项目：ChatRoom，start on：%s
+`
+)
+
+func main() {
+	fmt.Printf(banner+"\n", addr)
+	//server
 }
