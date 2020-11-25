2f88ec6b9b8c8c1a91f287ac52507ff40647d9db
周礼明 <zhouliming@shihuo.cn>
2020-11-25 09:58:23 +0800

    添加压测脚本

--- chatroom/cmd/benchmark/main.go
+++ chatroom/cmd/benchmark/main.go
@@ -1,8 +1,16 @@
 package benchmark
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+	"strconv"
 	"time"
+
+	"github.com/knight-zlm/chatroom/logic"
+	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
 )
 
 var (
@@ -18,3 +26,62 @@ func init() {
 }
 
 // 模拟用户登陆
+func main() {
+	flag.Parse()
+
+	for i := 0; i < userNum; i++ {
+		go UserConnect("user" + strconv.Itoa(i))
+		time.Sleep(loginInterval)
+	}
+}
+
+func UserConnect(nickname string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	conn, _, err := websocket.Dial(ctx, "ws://127.0.0.1:2020/ws?nickname="+nickname, nil)
+	if err != nil {
+		log.Println("Dial error:", err)
+		return
+	}
+	defer conn.Close(websocket.StatusInternalError, "内部错误！")
+
+	go sendMessage(conn, nickname)
+
+	ctx = context.Background()
+
+	for {
+		var message logic.Message
+		err = wsjson.Read(ctx, conn, &message)
+		if err != nil {
+			log.Println("receive msg error:", err)
+			continue
+		}
+
+		if message.ClientSendTime.IsZero() {
+			continue
+		}
+		if d := time.Now().Sub(message.ClientSendTime); d > 1*time.Second {
+			fmt.Printf("接收到服务器端的响应时间（%d）:%#v\n", d.Milliseconds(), message)
+		}
+	}
+
+	conn.Close(websocket.StatusNormalClosure, "")
+}
+
+func sendMessage(conn *websocket.Conn, nickname string) {
+	ctx := context.Background()
+	i := 1
+	for true {
+		msg := map[string]string{
+			"content":   "来自" + nickname + "的消息:" + strconv.Itoa(i),
+			"send_time": strconv.FormatInt(time.Now().UnixNano(), 10),
+		}
+		err := wsjson.Write(ctx, conn, msg)
+		if err != nil {
+			log.Println("send msg error:", err, "nickname", nickname, "no:", i)
+		}
+		i++
+		time.Sleep(msgInterval)
+	}
+}
