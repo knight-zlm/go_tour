b60da88b59a1b5c2aec46f9cd808b71b9e4cebd9
周礼明 <zhouliming@shihuo.cn>
2020-12-31 09:50:40 +0800

    添加性能测试

--- pprofUsing/demo3/main.go
+++ pprofUsing/demo3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
 	"runtime"
 	"runtime/pprof"
 )
@@ -19,7 +20,28 @@ const (
 )
 
 func pprofLookup(lookupType LookupType, w io.Writer) error {
-	return nil
+	var err error
+	switch lookupType {
+	case LookupGoroutine:
+		p := pprof.Lookup("goroutine")
+		err = p.WriteTo(w, 2)
+	case LookupThreadCreate:
+		p := pprof.Lookup("threadcreate")
+		err = p.WriteTo(w, 2)
+	case LookupHeap:
+		p := pprof.Lookup("heap")
+		err = p.WriteTo(w, 2)
+	case LookupAllocs:
+		p := pprof.Lookup("allocs")
+		err = p.WriteTo(w, 2)
+	case LookupBlock:
+		p := pprof.Lookup("block")
+		err = p.WriteTo(w, 2)
+	case LookupMutex:
+		p := pprof.Lookup("mutex")
+		err = p.WriteTo(w, 2)
+	}
+	return err
 }
 
 func init() {
@@ -29,6 +51,16 @@ func init() {
 
 func main() {
 	http.HandleFunc("/lookup/heap", func(w http.ResponseWriter, r *http.Request) {
-		_ = pprof.Lookup()
+		_ = pprofLookup(LookupHeap, os.Stdout)
 	})
+	http.HandleFunc("/lookup/threadcreate", func(w http.ResponseWriter, r *http.Request) {
+		_ = pprofLookup(LookupThreadCreate, os.Stdout)
+	})
+	http.HandleFunc("/lookup/block", func(w http.ResponseWriter, r *http.Request) {
+		_ = pprofLookup(LookupBlock, os.Stdout)
+	})
+	http.HandleFunc("/lookup/goroutine", func(w http.ResponseWriter, r *http.Request) {
+		_ = pprofLookup(LookupGoroutine, os.Stdout)
+	})
+	_ = http.ListenAndServe("0.0.0.0:6060", nil)
 }
