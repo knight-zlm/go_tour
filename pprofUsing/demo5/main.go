8eefe20ea2f7d1bca92dc85e423932b17a9a9c43
周礼明 <zhouliming@shihuo.cn>
2021-01-05 09:54:46 +0800

    新的测试案例

---
+++ pprofUsing/demo5/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "sync"
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(wg *sync.WaitGroup) {
+			var counter int
+			defer wg.Done()
+			for i := 0; i < 1e10; i++ {
+				counter++
+			}
+		}(&wg)
+	}
+	wg.Wait()
+}
