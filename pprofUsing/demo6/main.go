5ff9936fb4d85aa93023470552bfd84ca6d49548
周礼明 <zhouliming@shihuo.cn>
2021-01-07 10:00:57 +0800

    添加新的案例

---
+++ pprofUsing/demo6/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "sync"
+
+// GODEBUG=gctrace=1 go run main.go
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(wg *sync.WaitGroup) {
+			var counter int
+			for i := 0; i < 1e10; i++ {
+				counter++
+			}
+			wg.Done()
+		}(&wg)
+	}
+
+	wg.Wait()
+}
